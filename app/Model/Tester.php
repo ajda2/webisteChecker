0e9ab2b47c71f85949aed5571c39f7e27678799b
Michal Tichy <michal@tichy-vyvojar.cz>
2019-02-07 00:58:57 +0100

    Tester::runTests implements

--- app/Model/Tester.php
+++ app/Model/Tester.php
@@ -8,6 +8,7 @@ use Ajda2\WebsiteChecker\Model\Entity\TestResultInterface;
 use Ajda2\WebsiteChecker\Model\Entity\WebsiteInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\RequestOptions;
 use Masterminds\HTML5;
 use Nette\SmartObject;
 use Nette\Utils\ArrayHash;
@@ -18,7 +19,7 @@ use Psr\Http\Message\ResponseInterface;
  * @package Ajda2\WebsiteChecker\Model
  * @method onTestSuccess(Tester $tester, WebsiteInterface $website, TestInterface $test, TestResultInterface $testResult)
  * @method onTestFail(Tester $tester, WebsiteInterface $website, TestInterface $test, TestResultInterface $testResult)
- * @method onWebResponse(Tester $tester, WebsiteInterface $website, ResponseInterface $response)
+ * @method onWebResponse(Tester $tester, WebsiteInterface $website, ResponseInterface $response, float $responseTime)
  * @method onWebResponseFail(Tester $tester, WebsiteInterface $website)
  */
 class Tester {
@@ -64,11 +65,15 @@ class Tester {
 
 	public function runTests(WebsiteInterface $website, float $requestTimeout): bool {
 		$client = new Client();
+		$requestOptions = [RequestOptions::READ_TIMEOUT => $requestTimeout,];
 
 		try {
-			$response = $client->request('GET', (string)$website->getUrl(), ['timeout' => $requestTimeout,]);
+			$timeStart = \microtime(TRUE);
+			$response = $client->request('GET', (string)$website->getUrl(), $requestOptions);
+			$timeEnd = \microtime(TRUE);
+			$responseTime = $timeEnd - $timeStart;
 
-			$this->onWebResponse($this, $website, $response);
+			$this->onWebResponse($this, $website, $response, $responseTime);
 		} catch (GuzzleException $e) {
 			$this->onWebResponseFail($this, $website);
 
--- tests/Unit/Model/TesterTest.php
+++ tests/Unit/Model/TesterTest.php
@@ -4,22 +4,32 @@
 namespace Ajda2\WebsiteChecker\Tests\Unit\Model;
 
 use Ajda2\WebsiteChecker\Model\Entity\TestInterface;
+use Ajda2\WebsiteChecker\Model\Entity\TestResultInterface;
 use Ajda2\WebsiteChecker\Model\Entity\Website;
+use Ajda2\WebsiteChecker\Model\Entity\WebsiteInterface;
 use Ajda2\WebsiteChecker\Model\Tester;
 use Nette\Http\Url;
 use Nette\Utils\ArrayHash;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 class TesterTest extends TestCase {
 
 	/** @var Tester */
 	private $tester;
 
+	/** @var float */
+	private $requestTimeout = 0.0001;
+
+	/** @var bool */
+	private $eventFired;
+
 	public function setUp() {
 		parent::setUp();
 
 		$this->tester = new Tester();
+		$this->eventFired = FALSE;
 	}
 
 	public function test__construct() {
@@ -31,40 +41,118 @@ class TesterTest extends TestCase {
 
 	public function testRunTests() {
 		$code = 'test1';
-		$this->tester->addTest($this->createMockTest($code));
+		$this->tester->addTest($this->createMockTest($code, TRUE));
 		$website = new Website(0, new Url('https://www.surface.cz/'));
-		$requestTimeout = 4000.0;
 
-		$this->assertTrue($this->tester->runTests($website, $requestTimeout));
+		$this->assertTrue($this->tester->runTests($website, $this->requestTimeout));
 	}
 
 	public function testOnTestFail() {
-		$this->markTestSkipped();
+		$code = 'test1';
+		$website = new Website(0, new Url('https://www.zonaholesov.cz/admin/'));
+		$this->tester->addTest($this->createMockTest($code, FALSE));
+
+		$this->tester->onTestFail[] = function (
+			Tester $tester,
+			WebsiteInterface $website,
+			TestInterface $test,
+			TestResultInterface $testResult
+		): void {
+			$this->expectNotToPerformAssertions();
+			$this->eventFired = TRUE;
+
+			return;
+		};
+
+		$this->tester->runTests($website, $this->requestTimeout);
+
+		if (!$this->eventFired) {
+			$this->fail("Event 'onTestFail' was not triggered");
+		}
+	}
+
+	public function testOnTestSuccess() {
+		$code = 'test1';
+		$website = new Website(0, new Url('https://www.surface.cz/'));
+		$this->tester->addTest($this->createMockTest($code, TRUE));
+
+		$this->tester->onTestSuccess[] = function (
+			Tester $tester,
+			WebsiteInterface $website,
+			TestInterface $test,
+			TestResultInterface $testResult
+		): void {
+			$this->expectNotToPerformAssertions();
+			$this->eventFired = TRUE;
+
+			return;
+		};
+
+		$this->tester->runTests($website, $this->requestTimeout);
+
+		if (!$this->eventFired) {
+			$this->fail("Event 'onTestSuccess' was not triggered");
+		}
 	}
 
 	public function testOnWebResponse() {
-		$this->markTestSkipped();
+		$code = 'test1';
+		$website = new Website(0, new Url('https://www.surface.cz/'));
+		$this->tester->addTest($this->createMockTest($code, TRUE));
+
+		$this->tester->onWebResponse[] = function (
+			Tester $tester,
+			WebsiteInterface $website,
+			ResponseInterface $response,
+			float $responseTime
+		): void {
+			$this->expectNotToPerformAssertions();
+			$this->eventFired = TRUE;
+
+			return;
+		};
+
+		$this->tester->runTests($website, $this->requestTimeout);
+
+		if (!$this->eventFired) {
+			$this->fail("Event 'onWebResponse' was not triggered");
+		}
 	}
 
 	public function testOnWebResponseFail() {
-		$this->markTestSkipped();
+		$code = 'test1';
+		$website = new Website(0, new Url('https://www.notexists.cze/'));
+		$this->tester->addTest($this->createMockTest($code, TRUE));
+
+		$this->tester->onWebResponseFail[] = function (
+			Tester $tester,
+			WebsiteInterface $website
+		): void {
+			$this->expectNotToPerformAssertions();
+			$this->eventFired = TRUE;
+
+			return;
+		};
+
+		$this->tester->runTests($website, 10);
+
+		if (!$this->eventFired) {
+			$this->fail("Event 'onWebResponseFail' was not triggered");
+		}
 	}
 
 	public function testAddTest() {
 		$code = 'test1';
-		$this->tester->addTest($this->createMockTest($code));
+		$this->tester->addTest($this->createMockTest($code, TRUE));
 
 		$this->assertInstanceOf(ArrayHash::class, $this->tester->getTests());
 		$this->assertSame(1, $this->tester->getTests()->count());
 	}
 
-	public function testOnTestSuccess() {
-		$this->markTestSkipped();
-	}
 
 	public function testRemoveTest() {
 		$code = 'test1';
-		$this->tester->addTest($this->createMockTest($code));
+		$this->tester->addTest($this->createMockTest($code, TRUE));
 		$this->tester->removeTest($code);
 
 		$this->assertInstanceOf(ArrayHash::class, $this->tester->getTests());
@@ -73,11 +161,24 @@ class TesterTest extends TestCase {
 
 	/**
 	 * @param string $code
+	 * @param bool   $testSuccess
 	 * @return MockObject|TestInterface
 	 */
-	private function createMockTest(string $code): MockObject {
+	private function createMockTest(string $code, bool $testSuccess): MockObject {
 		$mock = $this->createMock(TestInterface::class);
 		$mock->method('getCode')->willReturn($code);
+		$mockResult = $this->createMock(TestResultInterface::class);
+
+		if ($testSuccess) {
+			$mockResult->method('isSuccess')->willReturn(TRUE);
+			$mockResult->method('isFail')->willReturn(FALSE);
+		} else {
+			$mockResult->method('isSuccess')->willReturn(FALSE);
+			$mockResult->method('isFail')->willReturn(TRUE);
+		}
+
+		$mock->method('run')->willReturn($mockResult);
+
 
 		return $mock;
 	}
