3059c30f1573871255dbeb5ab5d7eb84dde5a71b
Michal Tichy <michal@tichy-vyvojar.cz>
2019-02-27 17:45:35 +0100

    RobotsFile test added

---
+++ app/Model/Entity/TestCase/RobotsFile.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types = 1);
+
+namespace Ajda2\WebsiteChecker\Model\Entity\TestCase;
+
+
+use Ajda2\WebsiteChecker\Model\Entity\AbstractTest;
+use Ajda2\WebsiteChecker\Model\Entity\TestResult;
+use Ajda2\WebsiteChecker\Model\Entity\TestResultInterface;
+use Nette\Http\Url;
+use Nette\Utils\DateTime;
+use Nette\Utils\Strings;
+
+class RobotsFile extends AbstractTest {
+
+	/**
+	 * @param Url          $url
+	 * @param \DOMDocument $document
+	 * @return TestResultInterface
+	 * @throws \Exception
+	 */
+	public function run(Url $url, \DOMDocument $document): TestResultInterface {
+		if ($url->getPath() !== '/') {
+			return new TestResult($this->getCode(), new DateTime(), TRUE);
+		}
+
+		$robotsUrl = new Url((string)$url);
+		$robotsUrl->setPath('robots.txt');
+
+		try {
+			$fileContent = \file_get_contents((string)$robotsUrl);
+
+			if ($fileContent === FALSE) {
+				return new TestResult($this->getCode(), new DateTime(), FALSE, NULL, "Cannot read file");
+			}
+
+			if (Strings::contains(
+				"User-agent: *
+Disallow: /",
+				$fileContent
+			)) {
+				return new TestResult($this->getCode(), new DateTime(), FALSE, $fileContent, "All user agents are disabled");
+			}
+
+			return new TestResult($this->getCode(), new DateTime(), TRUE, $fileContent);
+		} catch (\Throwable $e) {
+			return new TestResult($this->getCode(), new DateTime(), FALSE, "test.robotsFile.file.notExists");
+		}
+	}
+}
---
+++ tests/Unit/Model/Entity/TestCase/RobotsFileTest.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types = 1);
+
+namespace Ajda2\WebsiteChecker\Tests\Unit\Model\Entity\TestCase;
+
+use Ajda2\WebsiteChecker\Model\Entity\TestCase\RobotsFile;
+use Ajda2\WebsiteChecker\Model\Entity\TestResultInterface;
+use Nette\Http\Url;
+use PHPUnit\Framework\TestCase;
+
+class RobotsFileTest extends TestCase {
+
+	/** @var RobotsFile */
+	private $item;
+
+	/** @var Url */
+	private $url;
+
+	public function setUp() {
+		parent::setUp();
+
+		$code = 'Code';
+		$name = 'Name';
+
+		$this->item = new RobotsFile($code, $name);
+		$this->url = new Url('https://www.surface.cz');
+	}
+
+	public function testConstructor(): void {
+		$code = 'Code';
+		$name = 'Name';
+
+		$this->item = new RobotsFile($code, $name);
+
+		$this->assertInstanceOf(RobotsFile::class, $this->item);
+		$this->assertSame($code, $this->item->getCode());
+		$this->assertSame($name, $this->item->getName());
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	public function testRunSuccess(): void {
+		$document = new \DOMDocument();
+
+		$result = $this->item->run($this->url, $document);
+
+		$this->assertInstanceOf(TestResultInterface::class, $result);
+		$this->assertTrue($result->isSuccess());
+		$this->assertFalse($result->isFail());
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	public function testRobotsDisabled(): void{
+		$document = new \DOMDocument();
+		$url = new Url('http://www.autodilyhorak.cz/');
+
+		$result = $this->item->run($url, $document);
+
+		$this->assertInstanceOf(TestResultInterface::class, $result);
+		$this->assertFalse($result->isSuccess());
+		$this->assertTrue($result->isFail());
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	public function testFileNotExists(): void {
+		$document = new \DOMDocument();
+		$url = new Url('http://tester.surface.cz');
+
+		$result = $this->item->run($url, $document);
+
+		$this->assertInstanceOf(TestResultInterface::class, $result);
+		$this->assertFalse($result->isSuccess());
+		$this->assertTrue($result->isFail());
+	}
+}
